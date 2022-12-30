3fa8f9863b62bae7f001db32871e92d6765bb47e
maulanasopian <maulanasopian12@gmail.com>
2022-12-30 14:32:11 +0700

    finish

--- react-list/src/App.js
+++ react-list/src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+
 import './App.css';
 
+
+function Car(props){
+  return <li>I am a {props.brand}</li>
+}
+function Garage(){
+  // const cars = ["bmw", "audi", "Ford"]
+  const cars = [
+    { id: 1, brand:"Bmw"},
+    { id: 2, brand:"Bmw2"},
+    { id: 3, brand:"Bmw3"},
+  ]
+  return(
+    <div>
+      <h1> Who lives in my garage</h1>
+      <ul>
+        {cars.map((car) => <Car key={car.id} brand={car.brand}></Car>)}
+      </ul>
+    </div>
+  );
+}
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Garage></Garage>
     </div>
   );
 }
