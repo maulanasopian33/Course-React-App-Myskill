ac2067cf48aeef06a01d1b908291d27eb5acf4f9
maulanasopian <maulanasopian12@gmail.com>
2022-12-30 12:32:12 +0700

    init

--- react-event/src/App.js
+++ react-event/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+
+function Football(){
+  const shoot = (a,b) => {
+    alert(b.type);
+  }
+
+  return (
+    <button onClick={(event) => shoot("Goal!",event)}> Take The shoot</button>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Football></Football>
     </div>
   );
 }
